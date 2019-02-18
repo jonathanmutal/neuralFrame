0c72ca77e5818bbc90b3945b3b2a76299af15f42
joni115 <Jonathan.Mutal@unige.ch>
2019-02-18 13:54:02 +0100

    adding moses pre-tokenizer

---
+++ preprocessing/tokenizer.py
@@ -0,0 +1,33 @@
+import mosestokenizer
+
+
+import mosestokenizer
+
+
+class Tokenizer:
+    """
+    This class will be a wrapper of MosesTokenizer.
+    """
+    def __init__(self, tokenizer=True, lang='en'):
+        """
+        :lang: lang for the tokenizer
+        :tokenize: true if you want to tokenize. Otherwise, it will be a detokenizer.
+        """
+        if tokenizer:
+            self.__procesor = mosestokenizer.MosesTokenizer(lang)
+        else:
+            self.__procesor = mosestokenizer.DeTokenizer(lang)
+
+    def tokenize_sentence(self, sentence):
+        """
+        :sentence: a string sentence.
+        retun a tokenized sentences.
+        """
+        sentence_processed = self.__procesor(sentence)
+        return ' '.join(sentence_processed)
+
+    def close(self):
+        """
+        Close the tokenizer
+        """
+        self.__procesor.close()
