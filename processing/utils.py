ee0b9ccd5bbfbcd9330443db7ea6277da99d57ae
joni115 <Jonathan.Mutal@unige.ch>
2019-02-20 14:24:15 +0100

    adding methods for posprocessing

--- processing/utils.py
+++ processing/utils.py
@@ -1,7 +1,7 @@
 import chardet
 
 
-def encoding_file(infile):
+def which_encoding(infile):
     """
     :infile: guess the infile encoding.
     return {'encoding': 'utf-8', 'confidence': 0.99, 'language': ''}
@@ -10,3 +10,20 @@ def encoding_file(infile):
         result = chardet.detect(rawdata.read(10000))
     # {'encoding': 'utf-8', 'confidence': 0.99, 'language': ''}
     return result['encoding']
+
+def remove_s(sentence):
+    return re.sub('</s>', '', sentence)
+
+def remove_spaces(sentence):
+    sentence = re.sub(' ,', ',', sentence)
+    sentence = re.sub('« ', '«', sentence)
+    sentence = re.sub(' »', '»', sentence)
+    sentence = re.sub('\' ', '\'', sentence)
+    sentence = re.sub(' :', ':', sentence)
+    sentence = re.sub(' °', '°', sentence)
+    sentence = re.sub(' - ', '-', sentence)
+    sentence = re.sub(' x ', 'x', sentence)
+    sentence = re.sub(' ’ ', '\'', sentence)
+    sentence = re.sub(' / ', '/', sentence)
+    sentence = re.sub('<\W*[0-9]*>', '', sentence)
+    return sentence
