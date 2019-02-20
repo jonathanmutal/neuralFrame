5c744bf2447974981cd0091147287d7f446be372
joni115 <Jonathan.Mutal@unige.ch>
2019-02-20 14:19:51 +0100

    dividing tokenizer from detokenizer

--- processing/tokenizer.py
+++ processing/tokenizer.py
@@ -1,30 +1,63 @@
 import mosestokenizer
 
+from processing.utils import remove_spaces
+
+"""
+TODO --- do a parent class.
+"""
 
 class Tokenizer:
     """
     This class will be a wrapper of MosesTokenizer.
     """
-    def __init__(self, tokenizer=True, lang='en'):
+    def __init__(self, lang='en'):
         """
         :lang: lang for the tokenizer
-        :tokenize: true if you want to tokenize. Otherwise, it will be a detokenizer.
         """
-        if tokenizer:
-            self.__procesor = mosestokenizer.MosesTokenizer(lang)
-        else:
-            self.__procesor = mosestokenizer.DeTokenizer(lang)
+        self.__tokenizer = mosestokenizer.MosesTokenizer(lang)
 
     def tokenize_sentence(self, sentence):
         """
         :sentence: a string sentence.
-        retun a tokenized sentences.
+        retun tokenized sentence.
         """
-        sentence_processed = self.__procesor(sentence)
-        return ' '.join(sentence_processed)
+        return ' '.join(self.__tokenizer(sentence))
 
     def close(self):
         """
         Close the tokenizer
         """
-        self.__procesor.close()
+        self.__tokenizer.close()
+
+
+class Detokenizer:
+    """
+    This class will be a wrapper of MosesTokenizer.
+    """
+    def __init__(self, lang='en'):
+        """
+        :lang: lang for the detokenizer
+        """
+        self.__detokenizer = mosestokenizer.DeTokenizer(lang)
+
+    def detokenize_sentence(self, sentence):
+        """
+        :sentence: a string sentence.
+        retun detokenized sentences.
+        """
+        sentence_processed = self.__detokenizer(sentence.split(' '))
+        sentence_processed = remove_spaces(sentence_processed)
+        return sentence_processed
+
+    def detokenize_sentences(self, sentences):
+        """
+        :sentences: a list of sentences
+        return a detokenized sentence
+        """
+        return [self.detokenize_sentence(sent) for sent in sentences]
+
+    def close(self):
+        """
+        Close the tokenizer
+        """
+        self.__detokenizer.close()
