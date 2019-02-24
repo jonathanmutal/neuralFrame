793535c72d066891feee544e1a535269209cb560
joni115 <Jonathan.Mutal@unige.ch>
2019-02-24 22:00:39 +0100

    adding new method for posprocessing neural sentences

--- processing/utils.py
+++ processing/utils.py
@@ -13,9 +13,17 @@ def which_encoding(infile):
     return result['encoding']
 
 def remove_s(sentence):
+    """
+    Remove all the ocurrence </s>.
+    :sentence: string
+    """
     return re.sub('</s>', '', sentence)
 
 def remove_spaces(sentence):
+    """
+    Remove all the spaces from a sentences.
+    :sentence: string.
+    """
     sentence = re.sub(' ,', ',', sentence)
     sentence = re.sub('« ', '«', sentence)
     sentence = re.sub(' »', '»', sentence)
@@ -28,3 +36,10 @@ def remove_spaces(sentence):
     sentence = re.sub(' / ', '/', sentence)
     sentence = re.sub('<\W*[0-9]*>', '', sentence)
     return sentence
+
+def neural_posprocessed(sentences):
+    """
+    The posprocessed needed for neural system.
+    :sentences: list of string
+    """
+    return list(map(lambda sent: remove_spaces(remove_s(sent)), sentences))
