f2c00c73d35aa129f5ef6f29a9ee992eb9901cd6
joni115 <Jonathan.Mutal@unige.ch>
2019-02-27 11:03:22 +0100

    upgrading index2words

--- embeddings/words.py
+++ embeddings/words.py
@@ -4,36 +4,54 @@ class IndexWords:
     A sentence would be understood by the algorithm as long as we map each word by 
     a number.
     """
-    def __init__(self, sentences, pad_word='<pad>'):
+    def __init__(self, sentences=[], vocabulary_file='', pad_word='<pad>', begin=0):
         """
         :sentences: the sentences which contains most vocabulary.
                     It has to be a list of all the sentences.
+        :vocabulary_file: pre-loaded vocabulary.
+        :pad_word: a word which will padded.
+        :begin: number you like to begin index
         """
-        assert len(sentences) != 0
-
         self.id2word = dict()
         self.word2id = dict()
         
         self.pad_word = pad_word
 
-        self.__create_index(sentences)
+        self.vocabulary_file = vocabulary_file
+        self.begin = begin
 
+        if sentences:
+            self.__create_vocabulary_from_sentences(sentences)
+        elif vocabulary_file:
+            self.__load_vocabulary()
 
-    def __create_index(self, sentences):
+    def __create_index(self, vocabulary):
+        """
+        :vocabulary: an order structure for indexing words.
+        """
+        for index, word in enumerate(vocabulary, self.begin):
+            self.id2word[index] = word
+            self.word2id[word] = index
+
+    def __load_vocabulary(self):
+        """
+        load the vocabulary from a file.
+        """
+        with open(self.vocabulary_file, 'r') as vocab:
+            vocabulary = map(lambda word: word.strip(), vocab.readlines())
+
+        self.__create_index(vocabulary)
+
+    def __create_vocabulary_from_sentences(self, sentences):
         """
         The main function where the magic comes up.
         """
         vocab = set()
         for sent in sentences:
             vocab.update(sent.split(" "))
+        vocabulary = [self.pad_word] + list(vocab)
 
-        # inicializate the dict with the pad_word
-        self.id2word[0] = self.pad_word
-        self.word2id[self.pad_word] = 0
-        # its the main loop which gives an index for each word
-        for index, word in enumerate(vocab):
-            self.id2word[index+1] = word
-            self.word2id[word] = index+1
+        self.__create_index(vocab)
 
     def get_vocabulary(self):
         return self.word2id.keys()
@@ -41,7 +59,6 @@ class IndexWords:
     def get_word(self, idx):
         """
         :idx: return a word from the idx.
-        ---
         Will return empty string if the idx is not in the dictionary
         """
         return self.word2id.get(idx, "")
@@ -49,7 +66,6 @@ class IndexWords:
     def get_index(self, word):
         """
         :word: return an id from a word.
-        ---
         Return -1 if the word is not in the dictionary
         """
         return self.word2id.get(word, -1)
