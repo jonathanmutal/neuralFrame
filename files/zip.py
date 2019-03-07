59c7a756e33d1a83e0bc17a7fa0ac110264add6b
Jonathan Mutal <jonathanmutal95@gmail.com>
2019-03-07 09:45:25 +0100

    fixing bugs and adding comentaries

--- files/zip.py
+++ files/zip.py
@@ -19,26 +19,37 @@ class Zip:
         zip_ref.extractall()
         zip_ref.close()
 
+        # path[:-len('.zip')]
         self.__outpath = os.path.join(path, '{0}_out.zip'.format(path[:-4]))
         self.__files =  zip_ref.filelist
         self.__config = configuration
+
+        self.__input_files = []
+        self.__output_files = []
+        for f in zip_ref.filelist:
+            dir_name = os.path.abspath(os.path.dirname(f.filename))
+            file_name = os.path.basename(f.filename)
+            abs_path = os.path.join(dir_name, file_name)
+            if not file_name:
+                continue
+            self.__input_files.append(abs_path)
+            self.__output_files.append('{0}_out.docx.xlf'.format(abs_path[:-9]))
 
     def generate_traduction(self):
         """
         generate the traduction for each folder.
         """
-        for f in self.__files:
-            dir_name = os.path.abspath(os.path.dirname(f.filename))
-            file_name = os.path.basename(f.filename)
-            abs_path = os.path.join(dir_name, file_name)
-            lang = 'fr' if 'fr' in dir_name else 'it'
-            Xliff(abs_path, self.__config, lang).put_translated_sentences('{0}_out.docx.xlf'.format(abs_path[:-9]))
-            os.remove(abs_path)
+        for f_in, f_out in zip(self.__input_files, self.__output_files): 
+            lang = 'fr' if 'fr' in f_in.lower() else 'it'
+            Xliff(f_in, self.__config, lang).put_translated_sentences(f_out)
 
 
     def generate_zip(self):
+        """
+        generate the final zip.
+        """
         zip_name = zipfile.ZipFile(self.__outpath, 'w')
-        for f in self.__files:
-            zip_name.write(f.filename[:-9] + '_out.docx.xlf')
-            shutil.rmtree(os.path.abspath(os.path.dirname(f.filename)))
+        for f_in, f_out in zip(self.__input_files, self.__output_files):
+            zip_name.write(f_out, os.path.join(os.path.basename(os.path.dirname(f_out)), os.path.basename(f_out)))
+            shutil.rmtree(os.path.abspath(os.path.dirname(f_in)))
         zip_name.close()
