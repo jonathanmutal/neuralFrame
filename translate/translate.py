dff4e61dc26b2a3b56167ee21af3379daebccf7c
Jonathan Mutal <jonathanmutal95@gmail.com>
2019-04-26 09:39:35 +0200

    adding a method for entities

--- translate/translate.py
+++ translate/translate.py
@@ -2,7 +2,7 @@ from processing.tokenizer import Tokenizer, Detokenizer
 from processing.subword import Subword
 from processing.truecase import TrueCase
 from translatorMT.neural import Neural
-from processing.utils import neural_posprocessed
+from processing.utils import neural_posprocessed, neural_preprocessing, recoverEntities
 
 
 class Translation:
@@ -10,19 +10,22 @@ class Translation:
     This class will be in charge of translation a sentence with all
     the preprocessing and posprocessing.
     """
-    def __init__(self, config, lang='en', tokenizer=True):
+    def __init__(self, config, lang='en'):
         """"
         All the files to do the pre-processing. First implementation.
+
+        - lang -- for the tokenizer.
         :config:
-            - lang -- for the tokenizer.
             - truecase -- the truecase file.
             - codesfile -- the codesfile.
             - config_path -- path from the configuration files.
             - model_type -- the model name.
         """
         self.__config = config[lang]
-        self.tokenizer = lambda sent: Tokenizer(lang).tokenize_sentence(sent) if tokenizer else id
-        self.detokenizer = Detokenizer(lang)
+        self.preprocessing = lambda sent: neural_preprocessing if self.__config['preprocessing'] else sent
+        self.posprocessing = lambda sents_s, sents_t: recoverEntities(sents_s, sents_t) if recoverEntities else sents_t
+        self.tokenizer = lambda sent: Tokenizer(lang).tokenize_sentence(sent) if self.__config['tokenizer'] else sent
+        self.detokenizer = lambda sent: Detokenizer(lang).detokenize_sentences(sent) if self.__config['tokenizer'] else sent
         self.truecaser = TrueCase(modelfile=self.__config.get('truecasemodel'))
         self.bpe = Subword(codesfile=self.__config.get('codesfile'))
         self.translator = Neural(self.__config)
@@ -35,15 +38,18 @@ class Translation:
         sent_proces = ''
         sentences_proces = []
         for sent in sentences:
-            sent_proces = self.tokenizer(sent)
-            sent_proces = self.truecaser.true_case_sentence(sent)
-            sent_proces = self.bpe.subword_sentence(sent) + '\n'
+            sent_proces = self.preprocessing(sent)
+            sent_proces = self.tokenizer(sent_proces)
+            sent_proces = self.truecaser.true_case_sentence(sent_proces)
+            sent_proces = self.bpe.subword_sentence(sent_proces) + '\n'
             sentences_proces.append(sent_proces)
         translated_sentences = self.translator.infer(sentences_proces)
         translated_sentences = self.bpe.de_subwords_sentences(translated_sentences)
         translated_sentences = self.truecaser.recaser_sentences(sentences, translated_sentences)
-        translated_sentences = self.detokenizer.detokenize_sentences(translated_sentences)
-        return neural_posprocessed(translated_sentences)
+        translated_sentences = self.detokenizer(translated_sentences)
+        translated_sentences = neural_posprocessed(translated_sentences)
+        translated_sentences = self.posprocessing(sentences, translated_sentences)
+        return translated_sentences
 
     def translate_file(self, path_in, path_out=''):
         """
